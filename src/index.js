0004188d009e0360c7ecb9103696983223934ef3
Max Page <maxpage@Maxs-MacBook-Pro.local>
2018-01-16 11:49:59 -0500

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Home from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Home />, document.getElementById('root'));
-registerServiceWorker();
